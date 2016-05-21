0cb0e2bf5b30037cc4efffa452787b688c0cad60
Masataka Kuwabara <p.ck.t22@gmail.com>
2016-05-21 12:34:14 +0900

    do pry

--- lib/pry_testcase.rb
+++ lib/pry_testcase.rb
@@ -1,5 +1,20 @@
 require "pry_testcase/version"
 
+require 'binding_of_caller'
+require 'pry'
+
 module PryTestcase
-  # Your code goes here...
+  @enabled = {}
+
+  class << self
+    def enabled?
+      @enabled[Thread.current]
+    end
+
+    def pry
+      return unless enabled?
+
+      binding.of_caller(1).pry
+    end
+  end
 end
